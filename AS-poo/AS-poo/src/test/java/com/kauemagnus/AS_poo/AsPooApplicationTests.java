812dc98893c594acb626d31f8e4fc7c86a0afea2
KaueMagnus <kmarquesmagnus@gmail.com>
2024-11-13 19:59:04 -0300

    add: projeto para AS

---
+++ AS-poo/AS-poo/src/test/java/com/kauemagnus/AS_poo/AsPooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kauemagnus.AS_poo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsPooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
