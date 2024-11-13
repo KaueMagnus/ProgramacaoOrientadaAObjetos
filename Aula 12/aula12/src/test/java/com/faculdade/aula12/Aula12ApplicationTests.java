5aa249b39bbbb46d7c79058ac009f97170e37684
KaueMagnus <kmarquesmagnus@gmail.com>
2024-11-13 19:57:58 -0300

    add: Aula 12

---
+++ Aula 12/aula12/src/test/java/com/faculdade/aula12/Aula12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faculdade.aula12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
